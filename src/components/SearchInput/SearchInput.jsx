96053a270f91d0dbd48e01ae3318d57df4851fc8
Andrey_Schulz <andreygorb040693@gmail.com>
2023-08-03 15:12:06 +0300

    add search component

---
+++ src/components/SearchInput/SearchInput.jsx
@@ -0,0 +1,62 @@
+import * as yup from "yup";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Formik, useFormikContext, Form, Field, ErrorMessage } from "formik";
+import {
+  selectError,
+  selectSelectedNumber,
+} from "../../store/ttn/ttn-selectors";
+import { fetchInfo } from "../../store/ttn/ttn-operation";
+
+const schema = yup.object().shape({
+  ttnNumber: yup
+    .string()
+    .matches(/^\d{14}$/, "Введіть 14 цифр")
+    .required("Поле обов'язкове для заповнення"),
+});
+
+const initialValues = {
+  ttnNumber: "",
+};
+const TtnInput = () => {
+  const { setFieldValue } = useFormikContext();
+
+  const ttnSelectedNumber = useSelector(selectSelectedNumber);
+
+  useEffect(() => {
+    setFieldValue("ttnNumber", ttnSelectedNumber);
+  }, [ttnSelectedNumber, setFieldValue]);
+  return (
+    <>
+      <Field type="text" name="ttnNumber" placeholder="Номер накладної" />
+      <ErrorMessage name="ttnNumber" component="div" />
+    </>
+  );
+};
+
+const SearchInput = () => {
+  let error = useSelector(selectError);
+  const dispatch = useDispatch();
+
+  const submitFetch = (values, { resetForm }) => {
+    dispatch(fetchInfo(values.ttnNumber));
+    resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={schema}
+      onSubmit={submitFetch}
+    >
+      <Form>
+        <div>
+          <TtnInput />
+        </div>
+        <button type="submit">Отримати статус</button>
+      </Form>
+    </Formik>
+  );
+};
+
+export default SearchInput;
